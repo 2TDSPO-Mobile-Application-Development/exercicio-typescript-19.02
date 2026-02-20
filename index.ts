a296b99df8c10d6e88ebb45f04e4ee84c93e2f83
hrqmartins <hrqmartins129@gmail.com>
2026-02-19 22:18:11 -0300

    feat: Criando o tipo Produto

---
+++ index.ts
@@ -0,0 +1,11 @@
+// https://whimsical.com/aula-19-02-2026-introducao-ao-typescript-78WUAi5YGbFCP8LeNFeYwi
+
+// 4.1 - Crie o type Produto
+type Produto = {
+  id: number;
+  nome: string;
+  preco: number;
+  categoria: string;
+  emEstoque: boolean;
+};
+
