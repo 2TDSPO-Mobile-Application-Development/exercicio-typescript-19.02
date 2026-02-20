9d04931f3748c44b09dd71f94ab25efbe31e626f
hrqmartins <hrqmartins129@gmail.com>
2026-02-19 22:18:39 -0300

    feat: Criando o array de produtos

--- index.ts
+++ index.ts
@@ -9,3 +9,27 @@ type Produto = {
   emEstoque: boolean;
 };
 
+// 4.2 - Crie o array de produtos
+const produtos: Produto[] = [
+  {
+    id: 1,
+    nome: "Samsung S24",
+    preco: 5999.0,
+    categoria: "Eletrônicos",
+    emEstoque: true,
+  },
+  {
+    id: 2,
+    nome: "Astronauta Luminária",
+    preco: 39.9,
+    categoria: "Eletrônicos",
+    emEstoque: false,
+  },
+  {
+    id: 3,
+    nome: "Camisa DryFit Nike",
+    preco: 349.9,
+    categoria: "Esportes",
+    emEstoque: true,
+  }
+];
